575080dc6b036ac0c338911b7312c41fda3cec50
YamaU10622 <tokuyama.yudai.jp@alumni.tsukuba.ac.jp>
2025-08-05 17:39:54 +0900

    first

---
+++ basic/ExplainCorporate.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class ExplainCorporate {
+  public static void main(String[] args){
+    System.out.println("***は***年に創業されました。");
+    System.out.println("コーポレートスローガンは\"***\"です。");
+  }
+}
